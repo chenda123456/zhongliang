53c5c107e3fc9122d285ddca8cd577712cecdcd0
chenda <c812843708@163.com>
2020-06-06 10:52:27 +0800

    0606第一次提交 list

---
+++ src/script/list.js
@@ -0,0 +1,9 @@
+! function($) {
+    const header = $('header');
+    header.load('./header.html');
+}(jQuery)
+
+! function($) {
+    const footer = $('footer');
+    footer.load('./footer.html');
+}(jQuery)
